816b47b86d89bdd42ce966176af07adc2046dc6a
Oscar Dub <oscardub@gmail.com>
2016-04-19 18:25:06 +0100

    Tidy rootReducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,7 @@ import { combineReducers } from 'redux';
 import activeNotes from './activeNotes';
 import channelScopes from './channelScopes';
 
-const rootReducer = combineReducers({
+export default combineReducers({
   channelScopes,
   activeNotes,
 });
-
-export default rootReducer;
