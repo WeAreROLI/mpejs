ba8591f4382e6a7612bff37a22e6e44b47795dd1
Oscar Dub <oscardub@gmail.com>
2016-04-19 17:11:45 +0100

    Set default noteState.

--- src/constants/defaults.js
+++ src/constants/defaults.js
@@ -5,12 +5,12 @@ export const PITCH_BEND = 8192;
 export const PRESSURE = 0;
 export const TIMBRE = 8192;
 export const NOTE_OFF_VELOCITY = 64;
-export const NOTE_STATE = noteStates.OFF;
+export const NOTE_STATE = noteStates.KEY_DOWN;
 
 export const ACTIVE_NOTE = {
   noteOnVelocity: NOTE_ON_VELOCITY,
   pitchBend: PITCH_BEND,
   pressure: PRESSURE,
   timbre: TIMBRE,
-  //noteState: NOTE_STATE,
+  noteState: NOTE_STATE,
 }
