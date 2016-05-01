88cd177bb15fd88f1e28b910a042c2e22879d5f3
Oscar Dub <oscardub@gmail.com>
2016-05-01 13:29:11 +0100

    Fix indentation in webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -6,102 +6,102 @@ var LIBRARY_NAME = 'WebMidiUtils';
 
 // Plugins
 var htmlPlugin = new HtmlWebpackPlugin({
-	filename: 'sandbox.html',
-	title: 'Web MIDI Sandbox',
-	template: './src/sandbox.html',
+  filename: 'sandbox.html',
+  title: 'Web MIDI Sandbox',
+  template: './src/sandbox.html',
 });
 var nodeProductionEnvPlugin = new webpack.DefinePlugin({
-	'process.env.NODE_ENV': '"production"'
+  'process.env.NODE_ENV': '"production"'
 });
 var uglifyPlugin = new webpack.optimize.UglifyJsPlugin({
-	compress: {
-		warnings: false,
-	}
+  compress: {
+    warnings: false,
+  }
 })
 
 // Loaders
 var babelLoader = {
-	test: /\.js$/,
-	loader: 'babel-loader',
-	query: {
-		// Using require.resolve to fix dependency issues when using `npm link`
-		presets: [require.resolve('babel-preset-es2015')]
-	}
+  test: /\.js$/,
+  loader: 'babel-loader',
+  query: {
+    // Using require.resolve to fix dependency issues when using `npm link`
+    presets: [require.resolve('babel-preset-es2015')]
+  }
 };
 
 // Env-specific options
 var env = process.env.WEBPACK_ENV;
 
 if (env === 'dev') {
-	var webpackConfig =	{
-		name: 'global',
-		devtool: 'source-map',
-		entry: {
-			WebMidiUtils: './src/global.js',
-			sandbox: './src/sandbox.js',
-		},
-		output: {
-			path: path.join(__dirname, 'lib'),
-			filename: '[name].js',
-			libraryTarget: 'var',
-			library: LIBRARY_NAME,
-		},
-		module: {
-			loaders: [babelLoader],
-		},
-		plugins: [htmlPlugin],
-	};
+  var webpackConfig = {
+    name: 'global',
+    devtool: 'source-map',
+    entry: {
+      WebMidiUtils: './src/global.js',
+      sandbox: './src/sandbox.js',
+    },
+    output: {
+      path: path.join(__dirname, 'lib'),
+      filename: '[name].js',
+      libraryTarget: 'var',
+      library: LIBRARY_NAME,
+    },
+    module: {
+      loaders: [babelLoader],
+    },
+    plugins: [htmlPlugin],
+  };
 }
 if (env === 'build') {
-	var webpackConfig = [
-		{
-			name: 'umd',
-			entry: { index: './src' },
-			output: {
-				path: path.join(__dirname, 'lib'),
-				filename: '[name].js',
-				libraryTarget: 'umd',
-				library: LIBRARY_NAME,
-				umdNamedDefine: true,
-			},
-			target: 'node',
-			module: {
-				loaders: [babelLoader],
-			},
-			plugins: [nodeProductionEnvPlugin],
-		},
-		{
-			name: 'global',
-			entry: {
-				WebMidiUtils: './src/global.js',
-				sandbox: './src/sandbox.js',
-			},
-			output: {
-				path: path.join(__dirname, 'lib'),
-				filename: '[name].js',
-				libraryTarget: 'var',
-				library: LIBRARY_NAME,
-			},
-			module: {
-				loaders: [babelLoader],
-			},
-			plugins: [htmlPlugin, nodeProductionEnvPlugin],
-		},
-		{
-			name: 'globalMinified',
-			entry: { WebMidiUtils: './src/global.js' },
-			output: {
-				path: path.join(__dirname, 'lib'),
-				filename: '[name].min.js',
-				libraryTarget: 'var',
-				library: LIBRARY_NAME,
-			},
-			module: {
-				loaders: [babelLoader],
-			},
-			plugins: [uglifyPlugin, nodeProductionEnvPlugin],
-		}
-	]
+  var webpackConfig = [
+    {
+      name: 'umd',
+      entry: { index: './src' },
+      output: {
+        path: path.join(__dirname, 'lib'),
+        filename: '[name].js',
+        libraryTarget: 'umd',
+        library: LIBRARY_NAME,
+        umdNamedDefine: true,
+      },
+      target: 'node',
+      module: {
+        loaders: [babelLoader],
+      },
+      plugins: [nodeProductionEnvPlugin],
+    },
+    {
+      name: 'global',
+      entry: {
+        WebMidiUtils: './src/global.js',
+        sandbox: './src/sandbox.js',
+      },
+      output: {
+        path: path.join(__dirname, 'lib'),
+        filename: '[name].js',
+        libraryTarget: 'var',
+        library: LIBRARY_NAME,
+      },
+      module: {
+        loaders: [babelLoader],
+      },
+      plugins: [htmlPlugin, nodeProductionEnvPlugin],
+    },
+    {
+      name: 'globalMinified',
+      entry: { WebMidiUtils: './src/global.js' },
+      output: {
+        path: path.join(__dirname, 'lib'),
+        filename: '[name].min.js',
+        libraryTarget: 'var',
+        library: LIBRARY_NAME,
+      },
+      module: {
+        loaders: [babelLoader],
+      },
+      plugins: [uglifyPlugin, nodeProductionEnvPlugin],
+    }
+  ]
 }
 
 module.exports = webpackConfig;
