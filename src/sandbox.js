66456d9fb83b60319dc876840362f4c1d9006114
Oscar Dub <oscardub@gmail.com>
2016-05-01 13:30:39 +0100

    Tidy sandbox js.

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,27 +1,37 @@
+/* eslint no-console: 0 */
+/* global WebMidiUtils: false */
+
 if (navigator.requestMIDIAccess) {
   console.log('\nWelcome to Web MIDI Sandbox\n\n');
 
   navigator.requestMIDIAccess({ sysex: true }).then(
-    function(x){
-      const inputs = Array.from(x.inputs.values());
-      const outputs = Array.from(x.outputs.values());
+    ({ inputMap, outputMap }) => {
+      const inputs = Array.from(inputMap.values());
+      const outputs = Array.from(outputMap.values());
       console.log('MIDI inputs:');
       console.log(inputs);
       console.log('MIDI outputs:');
       console.log(outputs);
-      if (!inputs) {
-	return 'No MIDI devices not found.';
+
+      if (!inputs && !outputs) {
+        return console.log('No MIDI devices not found.');
       }
-      const instrument = new WebMidiUtils.MpeInstrument(inputs[0], outputs[0]);
-      inputs[0].addEventListener('midimessage', ({ data }) =>
-		instrument.processMidiMessage(data)
+      const instrument = new WebMidiUtils.MpeInstrument();
+      const selectedInput = inputs[0];
+      selectedInput.addEventListener(
+        'midimessage',
+        ({ data }) => instrument.processMidiMessage(data)
       );
       instrument.debug();
+
+      const recorder = new WebMidiUtils.MidiRecorder(inputs[0]);
+      recorder.debug();
     },
-    function(error) {
+    (error) => {
       console.log('requestMIDIAccess failed.');
+      console.error(error);
     }
-  )
+  );
 } else {
   console.log('Please use a browser which supports Web MIDI API.');
 }
