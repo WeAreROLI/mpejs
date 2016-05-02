63686d5d71da02f694da7a96656d896d86f9d2b3
Oscar Dub <oscardub@gmail.com>
2016-05-02 13:42:54 +0100

    Add tests for channel scope messages.

--- test/mpeInstrument.test.js
+++ test/mpeInstrument.test.js
@@ -3,11 +3,17 @@ import { MpeInstrument } from '../lib';
 import 'chai';
 
 
-const NOTE_ON_1 = new Uint8Array([145, 60, 127]);
-const NOTE_ON_2 = new Uint8Array([146, 62, 127]);
-// const NOTE_OFF_1 = new Uint8Array([129, 60, 127]);
-const NOTE_OFF_2 = new Uint8Array([130, 62, 127]);
-const ZERO_VELOCITY_NOTE_OFF_1 = new Uint8Array([145, 60, 0]);
+const NOTE_ON_1 = new Uint8Array([0x92, 60, 127]);
+const NOTE_ON_2 = new Uint8Array([0x93, 62, 127]);
+const NOTE_ON_2_SAME_CHANNEL = new Uint8Array([0x93, 64, 127]);
+const NOTE_OFF_1_ZERO_VELOCITY = new Uint8Array([0x82, 60, 0]);
+const NOTE_OFF_2 = new Uint8Array([0x83, 62, 127]);
+
+const PITCH_BEND = new Uint8Array([0xe3, 127, 127]);
+const TIMBRE = new Uint8Array([0xb3, 74, 127]);
+const PRESSURE = new Uint8Array([0xd3, 127, 127]);
+
+const channelScopeMessages = { PITCH_BEND, TIMBRE, PRESSURE };
 
 describe('MpeInstrument', () => {
   let mpeInstrument;
@@ -28,10 +34,15 @@ describe('MpeInstrument', () => {
       mpeInstrument.processMidiMessage(NOTE_ON_1);
       expect(mpeInstrument.activeNotes().length).to.equal(1);
     });
+    it('can create two notes on the same channel', () => {
+      mpeInstrument.processMidiMessage(NOTE_ON_2);
+      mpeInstrument.processMidiMessage(NOTE_ON_2_SAME_CHANNEL);
+      expect(mpeInstrument.activeNotes().length).to.equal(2);
+    });
     it('treats a zero velocity note on as a note off', () => {
       // TODO: check noteOffVelocity.
       mpeInstrument.processMidiMessage(NOTE_ON_1);
-      mpeInstrument.processMidiMessage(ZERO_VELOCITY_NOTE_OFF_1);
+      mpeInstrument.processMidiMessage(NOTE_OFF_1_ZERO_VELOCITY);
       expect(mpeInstrument.activeNotes().length).to.equal(0);
     });
     it('removes specific activeNotes with note off', () => {
@@ -46,5 +57,25 @@ describe('MpeInstrument', () => {
       mpeInstrument.processMidiMessage(NOTE_ON_2);
       expect(mpeInstrument.activeNotes().length).to.equal(2);
     });
+
+    for (const key in channelScopeMessages) {
+      const messageName = key.toLowerCase().replace(/_/g, ' ');
+      const message = channelScopeMessages[key];
+      it(`${messageName} change affects all notes on channel`, () => {
+        mpeInstrument.processMidiMessage(NOTE_ON_2);
+        mpeInstrument.processMidiMessage(NOTE_ON_2_SAME_CHANNEL);
+        const state1 = mpeInstrument.activeNotes();
+        mpeInstrument.processMidiMessage(message);
+        const state2 = mpeInstrument.activeNotes();
+        expect(state1).not.to.deep.equal(state2);
+      });
+      it(`${messageName} change doesn't affect other channels`, () => {
+        mpeInstrument.processMidiMessage(NOTE_ON_1);
+        const state1 = mpeInstrument.activeNotes();
+        mpeInstrument.processMidiMessage(message);
+        const state2 = mpeInstrument.activeNotes();
+        expect(state1).to.deep.equal(state2);
+      });
+    }
   });
 });
