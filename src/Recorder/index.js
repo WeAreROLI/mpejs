733eb5b651ab45aa7e916acaf51c3f7e8bfa427d
Oscar Dub <oscardub@gmail.com>
2016-06-06 16:08:30 +0100

    Remove play method.

--- src/Recorder/index.js
+++ src/Recorder/index.js
@@ -1,7 +1,6 @@
 import * as actions from './actions';
 import rootReducer from './reducers';
 import { createStore } from 'redux';
-import { isRecordingStored } from './utils/recorderUtils';
 
 /* eslint no-console: 1 */
 export function createRecorder() {
@@ -10,13 +9,6 @@ export function createRecorder() {
 
   function record(message, time) {
     store.dispatch(actions.recordMessage(message, time));
-  }
-
-  function play() {
-    if (isRecordingStored(this.store.getState())) {
-      store.dispatch(actions.startPlayback());
-      return () => store.dispatch(actions.stopPlayback());
-    }
   }
 
   function dump() {
@@ -30,9 +22,8 @@ export function createRecorder() {
   /* eslint-enable no-console */
 
   return {
-    debug,
     record,
-    play,
     dump,
+    debug,
   };
 }
