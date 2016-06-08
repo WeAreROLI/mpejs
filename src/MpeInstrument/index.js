5c262e37d92de95c310dc6ddaf41b426e69218d9
Oscar Dub <oscardub@gmail.com>
2016-06-08 16:05:16 +0100

    Update MpeInstrument description.

--- src/MpeInstrument/index.js
+++ src/MpeInstrument/index.js
@@ -4,32 +4,33 @@ import { logger } from './middlewares';
 import rootReducer from './reducers';
 
 /**
-  * @summary Creates an MpeInstrument instance.
-  *
-  * MPE MIDI implementation details:
-  *
-  * Handles note messages on channels 1–16 as a single MPE zone.
-  *
-  * Channel 1 only implements "zone master channel" behaviour in the case of
-  * all notes off messages. Other messages types on channel 1 as standard
-  * channel scope messages.
-  *
-  * @param {Boolean} [options={ log: false }] Options to configure the recorder.
-  * @return {MpeInstrument} An MPE MIDI instrument instance.
-  */
-export function createMpeInstrument(options = { log: false }) {
-  
+ * Creates an MpeInstrument instance.
+ *
+ * MPE MIDI implementation details:
+ *
+ * Handles note messages on channels 1–16 as a single MPE zone.
+ *
+ * Channel 1 only implements "zone master channel" behaviour in the case of
+ * all notes off messages. Other messages types on channel 1 as standard
+ * channel scope messages.
+ *
+ * @param {Object} options Configuration options.
+ * @param {Boolean} [options.log=false] When true, logs active notes to the
+ * console.
+ * @return {MpeInstrument} An MPE MIDI instrument instance.
+ */
+export default function MpeInstrument(options = { log: false }) /** @lends MpeInstrument.prototype */ {
   const store = options.log ?
     createStore(rootReducer, applyMiddleware(logger)) :
     createStore(rootReducer);
-
   /**
    * Reads MIDI message data and updates the instrument state accordingly.
    *
    * Processed MIDI messages trigger changes or modulations in `activeNotes`.
    *
    * @param {Uint8Array} midiMessage A MIDI message.
-   * @returns {undefined}
+   * @return {undefined}
+   * @method processMidiMessage
    */
   function processMidiMessage(midiMessage) {
     const actions = generateMidiActions(midiMessage, store.getState);
