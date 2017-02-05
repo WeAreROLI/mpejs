e94980cfba9e21b30db18a9d95be7e254956cf53
Oscar Dub <oscar@roli.com>
2017-02-05 19:07:54 +0000

    Switch to unicode sharps and flats.

--- test/unit/noteNumberUtils.test.js
+++ test/unit/noteNumberUtils.test.js
@@ -3,14 +3,14 @@ import { toScientificPitch, toHelmholtzPitch } from '../../src/mpeInstrument/uti
 import noteNumberToPitch from '../data/noteNumberToPitch';
 
 describe('noteNumberUtils', () => {
-  describe('toScientificPitch', () => {
+  describe('toScientificPitch()', () => {
     noteNumberToPitch.forEach(({ noteNumber, scientific }, i) => {
       it(`converts note number ${noteNumber} to ${scientific}`, () => {
         expect(toScientificPitch(noteNumber)).to.eq(scientific);
       });
     });
   });
-  describe('toHelmholtzPitch', () => {
+  describe('toHelmholtzPitch()', () => {
     noteNumberToPitch.forEach(({ noteNumber, helmholtz }, i) => {
       it(`converts note number ${noteNumber} to ${helmholtz}`, () => {
         expect(toHelmholtzPitch(noteNumber)).to.eq(helmholtz);
