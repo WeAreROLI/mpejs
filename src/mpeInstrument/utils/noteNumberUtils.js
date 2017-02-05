91f5149fdac6c6f1aa68deac242554517ae24ea9
Oscar Dub <oscar@roli.com>
2017-02-05 15:58:33 +0000

    Implement toScientificPitch.

--- src/mpeInstrument/utils/noteNumberUtils.js
+++ src/mpeInstrument/utils/noteNumberUtils.js
@@ -1,7 +1,34 @@
+const PITCH_CLASS_NUMBER_TO_PITCH_NAME = {
+  0:  'C',
+  1:  'C#',
+  2:  'D',
+  3:  'Eb',
+  4:  'E',
+  5:  'F',
+  6:  'F#',
+  7:  'G',
+  8:  'Ab',
+  9:  'A',
+  10: 'Bb',
+  11: 'B',
+};
+
+export function toPitchClassNumber(noteNumber) {
+  return Math.floor(noteNumber % 12);
+}
+
+export function toOctaveNumber(noteNumber) {
+  return Math.floor(noteNumber / 12);
+}
+
+export function toPitchClassName(noteNumber) {
+  return PITCH_CLASS_NUMBER_TO_PITCH_NAME[toPitchClassNumber(noteNumber)];
+}
+
 export function toHelmholtzPitch(noteNumber) {
   return `helmholtz ${noteNumber}`;
 }
 
 export function toScientificPitch(noteNumber) {
-  return `scientific ${noteNumber}`;
+  return `${toPitchClassName(noteNumber)}${toOctaveNumber(noteNumber)}`;
 }
