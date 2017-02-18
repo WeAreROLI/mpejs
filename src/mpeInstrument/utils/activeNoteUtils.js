cfcaa2a153f9faa97530ef8d74fa6607292e351a
Oscar Dub <oscar@roli.com>
2017-02-18 14:35:42 +0000

    Add int to float conversion utilities.

--- src/mpeInstrument/utils/activeNoteUtils.js
+++ src/mpeInstrument/utils/activeNoteUtils.js
@@ -1,11 +1,23 @@
 import { transformObject } from './objectUtils';
 
+export const int7ToSignedFloat = v => v <= 64
+  ? 0.5 * v / 64
+  : v / 127;
+
+export const int14ToUnsignedFloat = v => v <= 8192
+  ? 0.5 * v / 8192
+  : v / 16383;
+
+export const int14ToSignedFloat = v => v <= 8192
+  ? v / 8192 - 1
+  : (v - 8192) / 8191;
+
 const NORMALIZE_NOTE_TRANSFORMATIONS = {
-  noteOnVelocity: v => v / 127,
-  noteOffVelocity: v => v / 127,
-  pitchBend: v => (2 * v / 16383) - 1,
-  pressure: v => v / 16383,
-  timbre: v => v / 16383,
+  noteOnVelocity: int7ToSignedFloat,
+  noteOffVelocity: int7ToSignedFloat,
+  pitchBend: int14ToSignedFloat,
+  pressure: int14ToUnsignedFloat,
+  timbre: int14ToUnsignedFloat,
 };
 
 export function normalizeNote(note) {
